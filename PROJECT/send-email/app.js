0ceb661d34f82686b0594113518d6a5f257de124
Radhe Krishna <htank8866@gmail.com>
2024-06-12 15:50:52 +0530

    add project

---
+++ PROJECT/send-email/app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+
+const sendEmail = require('./controllers/sendEmail')
+
+//error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>Email Project</h1> <a href="/send">send email</a>');
+});
+
+app.get('/send',sendEmail) 
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
