a90e0a62ed90406bd28a04e690eb59299dde2f13
Radhe Krishna <htank8866@gmail.com>
2024-06-18 15:31:47 +0530

    update code

--- PROJECT/file-upload/app.js
+++ PROJECT/file-upload/app.js
@@ -1,39 +1,46 @@
-require('dotenv').config()
-require('express-async-errors')
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+const fileUpload = require('express-fileupload')
+
 
 //database
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
 
 // product router
-const productRouter = require('./routes/productRoutes')
-
-
+const productRouter = require("./routes/productRoutes");
 
 //error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.get('/',(req,res)=>{
-    res.send('<h1>File Upload Starter </h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>File Upload Starter </h1>");
+});
 
-app.use(express.json())
+app.use(express.json());
+app.use(fileUpload())
 
 
-app.use('/api/v1/products',productRouter)
+app.use("/api/v1/products", productRouter);
 //MIDDLEWARE
 // app.use(notFoundMiddleware)
 // app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 3000;
- 
+
 const start = async () => {
   try {
-     // connectDB
-     console.log('MongoDB URI:', process.env.MONGO_URI);
+    // connectDB
+    // await connectDB(process.env.MONGO_URI);
+    // console.log('Connected to MongoDB');
+
+    console.log("MongoDB URI:", process.env.MONGO_URI);
+   
+   
     app.listen(port, () =>
       console.log(` File Upload Server is listening on port ${port}...`)
     );
---
+++ PROJECT/file-upload/db/connect.js
@@ -0,0 +1,11 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = (url) => {
+//   return mongoose.connect(url, {
+//     maxPoolSize: 10, // Use maxPoolSize
+//     serverSelectionTimeoutMS: 30000,
+//     socketTimeoutMS: 45000
+//   });
+// };
+
+// module.exports = connectDB;
