57bd35d9f43ff2d38b0f80542d99babb5b931b74
Radhe Krishna <htank8866@gmail.com>
2024-06-21 16:11:52 +0530

    AUTH_TUT project complate

--- PROJECT/AUTH_TUT/helpers/generate_keys.js
+++ PROJECT/AUTH_TUT/helpers/generate_keys.js
@@ -1,6 +1,8 @@
 
+const { log } = require('console')
 const crypto = require('crypto')
 
 const key1 = crypto.randomBytes(32).toString('hex')
+
 const key2 = crypto.randomBytes(32).toString('hex')
 console.table({ key1, key2 })
--- PROJECT/AUTH_TUT/helpers/init_redis.js
+++ PROJECT/AUTH_TUT/helpers/init_redis.js
@@ -1,28 +1,34 @@
-const redis = require('redis')
+const redis = require('redis');
 
+// Create a Redis client
 const client = redis.createClient({
-  port: 6379,
-  host: '127.0.0.1',
-})
+  port: 6379, // Redis port
+  host: '127.0.0.1', // Redis host
+});
 
+// Event listener for successful connection
 client.on('connect', () => {
-  console.log('Client connected to redis...')
-})
+  console.log('Client connected to redis...');
+});
 
+// Event listener when the client is ready to use
 client.on('ready', () => {
-  console.log('Client connected to redis and ready to use...')
-})
+  console.log('Client connected to redis and ready to use...');
+});
 
+// Event listener for errors
 client.on('error', (err) => {
-  console.log(err.message)
-})
+  console.log(err.message);
+});
 
+// Event listener for disconnection
 client.on('end', () => {
-  console.log('Client disconnected from redis')
+  console.log('Client disconnected from redis');
 })
 
+// Gracefully handle process termination
 process.on('SIGINT', () => {
-  client.quit()
-})
+  client.quit();
+});
 
-module.exports = client
+module.exports = client;
